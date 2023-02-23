29861ec6b3ccedcf7cdd747ccf1d61c580b8a61c
YJ <1312691968@qq.com>
2023-02-23 13:25:18 +0800

    第一次提交

---
+++ s2_spring_ioc_xml/src/test/java/com/yj/spring/test/GitTest.java
@@ -0,0 +1,14 @@
+package com.yj.spring.test;
+
+/**
+ * Description:
+ *
+ * @Author YJ
+ * @Create 2023/2/23 13:20
+ * @Version 1.0
+ */
+public class GitTest {
+   public static void main(String[] args) {
+      System.out.println("Hello Git!");
+   }
+}
